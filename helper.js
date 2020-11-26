8747e34da5fc37e81d4000afa6aa2fdfbbd74059
richardpassos01 <richard.passos@beetech.digital>
2020-11-26 18:50:54 -0300

    feat: use batch to create/update and archive contacts on hubspot

---
+++ helper.js
@@ -0,0 +1,37 @@
+const hubspot = require('@hubspot/api-client');
+const API_KEY = process.env.HUBSPOT_API_KEY;
+
+let hubspotClient;
+
+const getHubspotInstance = () => {
+    hubspotClient = hubspotClient || new hubspot.Client({ apiKey: API_KEY });
+    return hubspotClient;
+}
+
+const createContactOnHubspot = async (contacts) => {
+    const hubspotClient = getHubspotInstance();
+
+    return hubspotClient.crm.contacts.batchApi.create(contacts)
+}
+
+const updateContactOnHubspot = async (contacts) => {
+    const hubspotClient = getHubspotInstance();
+
+    return hubspotClient.crm.contacts.batchApi.update(contacts)
+}
+
+const deleteContactOnHubspot = async (contactsHubspot, contactsMongo, contactLightCollection) => {
+    const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
+
+    const archiveContactsOnHub = await hubspotClient.crm.contacts.batchApi.archive(contactsHubspot);
+
+    return contactLightCollection.remove({
+        id_customer: { $in: contactsMongo }
+    });
+}
+
+module.exports = {
+    createContactOnHubspot,
+    updateContactOnHubspot,
+    deleteContactOnHubspot
+}
--- index.js
+++ index.js
@@ -1,265 +1,121 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const PORT = process.env.PORT;
+
+const {
+    createContactOnHubspot,
+    updateContactOnHubspot,
+    deleteContactOnHubspot,
+} = require('./helper');
 
 const app = express();
-
-const hubspot = require('@hubspot/api-client');
-const fs = require('fs');
-
-const PORT = 3000;
-const API_KEY = '';
-
 app.use(express.json());
 
-app.post('/hubspot', async (req, res) => {
+app.post('/hubspot/batch', async (req, res) => {
     try {
-        const contactObj = { 
-            properties: {
-                firstname: 'richssards',
-                lastname: 'ppssp',
-                email: 'rickssss@mastercheff.com'
-            }
+        const messages = req.body;
+        const customerIds = [];
+        const customersToUpdate = {
+            inputs: []
         };
+
+        const customerToCreate = {
+            inputs: []
+        };
+        const customersToDeleteOnHubspot = {
+            inputs: []
+        };
+        const customersToDeleteOnMongoDb = [];
+
+        const connection = await MongoClient.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    
+        const db = await connection.db(process.env.MONGO_DATABASE);
+        const [contactLightCollection] = await Promise.all(
+            ['contact_light'].map(collection => db.collection(collection)),
+        );
+
+        if(messages.delete && messages.delete.length) {
+            customerIds.push(...messages.delete); 
+        }
+
+        if(messages.createOrUpdate && messages.createOrUpdate.length) {
+            messages.createOrUpdate.forEach(operation => {
+                customerIds.push(operation.id_customer);
+            });
+        }
+
+        const usersFromMongoDb = await contactLightCollection.find(
+            {
+                id_customer: {
+                    $in: customerIds
+                }
+            },
+            {
+                projection: {
+                    hubspotCustomerId: 1,
+                    id_customer: 1,
+                    _id: 0
+                }
+            }
+        ).toArray();
+
+        if(messages.createOrUpdate && messages.createOrUpdate.length) {
+            messages.createOrUpdate.forEach((customer) => {
+                const user = usersFromMongoDb.find((mongoUser) => 
+                    mongoUser.id_customer === customer.id_customer);
+
+                if(user) {
+                    return customersToUpdate.inputs.push({
+                        id: user.hubspotCustomerId,
+                        properties: customer.properties
+                    });
+                }
+
+                return customerToCreate.inputs.push({
+                    properties: customer.properties
+                })
+            });
+        }
+          
+        if(messages.delete && messages.delete.length) {
+            messages.delete.forEach((idCustomer) => {
+                const user = usersFromMongoDb.find((mongoUser) => 
+                     mongoUser.id_customer === idCustomer);
+
+                if(user) {
+                    customersToDeleteOnHubspot.inputs.push({
+                        id: user.hubspotCustomerId,
+                    });
+
+                    customersToDeleteOnMongoDb.push(idCustomer);
+                }
+            });
+        }
+
+        if(customersToDeleteOnHubspot.inputs.length) {
+            await deleteContactOnHubspot(
+                customersToDeleteOnHubspot,
+                customersToDeleteOnMongoDb,
+                contactLightCollection,
+            )
+        }
+
+        if(customerToCreate.inputs.length) {
+            await createContactOnHubspot(customerToCreate)
+        }
         
-        const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
-        const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(contactObj)
-        
+        if(customersToUpdate.inputs.length) {
+            await updateContactOnHubspot(customersToUpdate)
+        }
+
         res.send(200);
     } catch (error) {
         console.error(error)
     }
 });
-
-app.put('/hubspot/:customerId', async (req, res) => {
-    try {
-        const { customerId } = req.params;
-
-        const contactObj = { 
-            properties: {
-                firstname: 'eduardo',
-                lastname: 'semname',
-                email: 'exxx@mastercheff.com'
-            }
-        };
-
-        const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
-        const updateResponse = await hubspotClient.crm.contacts.basicApi.update(customerId, contactObj)
-            
-        res.send(200);
-    } catch (error) {
-        console.error(error)
-    }
-});
-
-app.post('/imports', async (req, res) => {
-    try {
-        const fileName = 'example.csv';
-
-        const file = fs.readFileSync(fileName);
-        // console.log(file)
-        // const fileToImport = _.get(req, 'files.file')
-
-        if (!file) {
-            throw new Error('Cannot get file to import')
-        }
-
-        const fileToImportConfig = {
-            value: file,
-            options: {
-                filename: fileName,
-                contentType: 'text/csv',
-            },
-        }
-
-        const importRequest = {
-            name: fileName,
-            files: [
-                {
-                    fileName: fileName,
-                    fileImportPage: {
-                        hasHeader: true,
-                        columnMappings: [
-                            {
-                                columnName: 'First Name',
-                                propertyName: 'firstname',
-                                columnObjectType: 'CONTACT',
-                            },
-                            {
-                                columnName: 'last Name',
-                                propertyName: 'lastname',
-                                columnObjectType: 'CONTACT',
-                            },
-                            {
-                                columnName: 'Email',
-                                propertyName: 'email',
-                                columnObjectType: 'CONTACT',
-                            },
-                        ],
-                    },
-                },
-            ],
-        }
-
-        // Start a new import
-        // POST/crm/v3/imports
-        // https://developers.hubspot.com/docs/api/crm/imports
-        console.log('Calling crm.imports.coreApi.create API method. Starting a new import')
-        const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
-        const result = await hubspotClient.crm.imports.coreApi.create(JSON.stringify(importRequest), fileToImportConfig)
-
-        // logResponse('Import result', result)
-        // res.redirect(`imports/${result.body.id}`)
-        res.sendStatus(200);
-    } catch (e) {
-        console.log(e)
-    }
-})
-
-
-app.post('/imports/deal', async (req, res) => {
-    try {
-        const fileName = 'hub-deals.csv';
-
-        const file = fs.readFileSync(fileName);
-        // console.log(file)
-        // const fileToImport = _.get(req, 'files.file')
-
-        if (!file) {
-            throw new Error('Cannot get file to import')
-        }
-
-        const fileToImportConfig = {
-            value: file,
-            options: {
-                filename: fileName,
-                contentType: 'text/csv',
-            },
-        }
-
-        const importRequest = {
-            name: fileName,
-            files: [
-                {
-                    fileName: fileName,
-                    fileImportPage: {
-                        hasHeader: true,
-                        columnMappings: [
-                            {
-                                columnName: 'Deal Name',
-                                propertyName: 'Deal Name',
-                                columnObjectType: 'DEAL',
-                            },
-                            {
-                                columnName: 'Deal Stage',
-                                propertyName: 'Deal Stage',
-                                columnObjectType: 'DEAL',
-                            },
-                            {
-                                columnName: 'Amount',
-                                propertyName: 'Amount',
-                                columnObjectType: 'DEAL',
-                            },
-                            {
-                                columnName: 'Close Date',
-                                propertyName: 'Close Date',
-                                columnObjectType: 'DEAL',
-                            },
-                            {
-                                columnName: 'Product of Interest',
-                                propertyName: 'Product of Interest',
-                                columnObjectType: 'DEAL',
-                            },
-                            {
-                                columnName: 'Point of Contact',
-                                propertyName: 'Point of Contact',
-                                columnObjectType: 'DEAL',
-                            }
-                        ],
-                    },
-                },
-            ],
-        }
-
-        // Start a new import
-        // POST/crm/v3/imports
-        // https://developers.hubspot.com/docs/api/crm/imports
-        console.log('Calling crm.imports.coreApi.create API method. Starting a new import')
-        const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
-        const result = await hubspotClient.crm.imports.coreApi.create(JSON.stringify(importRequest), fileToImportConfig)
-
-        // logResponse('Import result', result)
-        // res.redirect(`imports/${result.body.id}`)
-        res.sendStatus(200);
-    } catch (e) {
-        console.log(e)
-    }
-})
-
-
-
-app.put('/imports', async (req, res) => {
-    try {
-        const fileName = 'example2.csv';
-
-        const file = fs.readFileSync(fileName);
-        // console.log(file)
-        // const fileToImport = _.get(req, 'files.file')
-
-        if (!file) {
-            throw new Error('Cannot get file to import')
-        }
-
-        const fileToImportConfig = {
-            value: file,
-            options: {
-                filename: fileName,
-                contentType: 'text/csv',
-            },
-        }
-
-        const importRequest = {
-            name: fileName,
-            files: [
-                {
-                    fileName: fileName,
-                    fileImportPage: {
-                        hasHeader: true,
-                        columnMappings: [
-                            {
-                                columnName: 'First Name',
-                                propertyName: 'firstname',
-                                columnObjectType: 'CONTACT',
-                            },
-                            {
-                                columnName: 'Last Name',
-                                propertyName: 'lastname',
-                                columnObjectType: 'CONTACT',
-                            },
-                            {
-                                columnName: 'Email',
-                                propertyName: 'email',
-                                columnObjectType: 'CONTACT',
-                            },
-                        ],
-                    },
-                },
-            ],
-        }
-
-        // Start a new import
-        // POST/crm/v3/imports
-        // https://developers.hubspot.com/docs/api/crm/imports
-        console.log('Calling crm.imports.coreApi.create API method. Starting a new import')
-        const hubspotClient = new hubspot.Client({ apiKey: API_KEY });
-        const result = await hubspotClient.crm.imports.coreApi.create(JSON.stringify(importRequest), fileToImportConfig)
-
-        // logResponse('Import result', result)
-        // res.redirect(`imports/${result.body.id}`)
-        res.sendStatus(200);
-    } catch (e) {
-        console.log(e)
-    }
-})
-
 
 app.listen(PORT, () => {
     console.log(`app listen o port ${PORT}`)
